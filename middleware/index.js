7158dbcb9cc58ac03daf10d43c8c5f50d032526d
Vignesh-02 <laserlikefocus000@gmail.com>
2026-01-29 18:45:30 +0530

    created version 2

---
+++ middleware/index.js
@@ -0,0 +1,67 @@
+const Location = require("../models/location");
+const Comment = require("../models/comment");
+
+// All middleware goes here
+const middlewareObj = {};
+
+middlewareObj.checkLocationOwnership = async (req, res, next) => {
+    if (req.isAuthenticated()) {
+        try {
+            const foundLocation = await Location.findById(req.params.id);
+            if (!foundLocation) {
+                req.flash("error", "Location not found");
+                return res.redirect("back");
+            }
+            // Does user own the location?
+            if (foundLocation.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+        } catch (err) {
+            console.log(err);
+            req.flash("error", "Location not found");
+            res.redirect("back");
+        }
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+};
+
+middlewareObj.checkCommentOwnership = async (req, res, next) => {
+    if (req.isAuthenticated()) {
+        try {
+            const foundComment = await Comment.findById(req.params.comment_id);
+            if (!foundComment) {
+                req.flash("error", "Comment not found");
+                return res.redirect("back");
+            }
+            // Does user own the comment?
+            if (foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+        } catch (err) {
+            console.log(err);
+            req.flash("error", "Comment not found");
+            res.redirect("back");
+        }
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+};
+
+middlewareObj.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("/login");
+};
+
+module.exports = middlewareObj;
