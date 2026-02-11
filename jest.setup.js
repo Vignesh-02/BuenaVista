2d475a971f1fdaada50672f6e597d72015e6b5b1
Vignesh-02 <laserlikefocus000@gmail.com>
2026-02-11 14:53:37 +0530

    removed unused variable mongod

--- jest.setup.js
+++ jest.setup.js
@@ -7,10 +7,8 @@ process.env.NODE_ENV = "test";
 
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
-let mongod;
 global.mongoPromise = MongoMemoryServer.create()
     .then((m) => {
-        mongod = m;
         const uri = m.getUri();
         process.env.MONGODB_URL = uri;
         global.TEST_MONGODB_URI = uri; // So app.js can restore it after loading .env
