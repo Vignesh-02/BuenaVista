224cd51e8fb0ee46ed58806fdc3f6124b9502e32
Vignesh-02 <laserlikefocus000@gmail.com>
2026-02-18 14:46:22 +0530

    add favicon and script to resize it

---
+++ scripts/generate-favicon-png.js
@@ -0,0 +1,28 @@
+/**
+ * Generates favicon.png from favicon.svg with transparent background.
+ * Run from project root: node scripts/generate-favicon-png.js
+ */
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
+
+const publicDir = path.join(__dirname, "..", "public");
+const svgPath = path.join(publicDir, "favicon.svg");
+const outPath = path.join(publicDir, "favicon.png");
+
+if (!fs.existsSync(svgPath)) {
+  console.error("favicon.svg not found in public/");
+  process.exit(1);
+}
+
+sharp(svgPath)
+  .resize(32, 32)
+  .png()
+  .toFile(outPath)
+  .then((info) => {
+    console.log("Generated public/favicon.png with transparent background", info);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
