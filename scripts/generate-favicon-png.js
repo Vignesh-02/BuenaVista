6b152e0060307cbc4c7aa1a5d9f2b0a5bdd9c7aa
Vignesh <vignesh@turnkey.work>
2026-02-18 14:53:05 +0530

    fix format with prettier

--- scripts/generate-favicon-png.js
+++ scripts/generate-favicon-png.js
@@ -11,18 +11,18 @@ const svgPath = path.join(publicDir, "favicon.svg");
 const outPath = path.join(publicDir, "favicon.png");
 
 if (!fs.existsSync(svgPath)) {
-  console.error("favicon.svg not found in public/");
-  process.exit(1);
+    console.error("favicon.svg not found in public/");
+    process.exit(1);
 }
 
 sharp(svgPath)
-  .resize(32, 32)
-  .png()
-  .toFile(outPath)
-  .then((info) => {
-    console.log("Generated public/favicon.png with transparent background", info);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+    .resize(32, 32)
+    .png()
+    .toFile(outPath)
+    .then((info) => {
+        console.log("Generated public/favicon.png with transparent background", info);
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
